734bbc86fa727a2518b0ee48876ec05ac00bec37
Kiril Varabei <kiril_varabei@epam.com>
2023-11-18 22:19:51 +0100

    feat: add react-router configuration, create basic folders structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Search } from './pages/Search.page';
+import { Media } from './pages/Media.page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Search />,
+  },
+  {
+    path: "/media/:mediaId",
+    element: <Media />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
