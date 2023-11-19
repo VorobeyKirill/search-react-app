4a80efe86630f996678f3ef299b0bde8e8306940
Kiril Varabei <kiril_varabei@epam.com>
2023-11-19 13:31:01 +0100

    style: add sass package to the project, add styles for SearchBar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from "react";
+import "./SearchBar.scss";
 
 interface ISearchBarProps {
     handleSearchBarSubmit: (event: FormEvent<HTMLFormElement>, searchValue: string) => Promise<void>
@@ -7,14 +8,22 @@ interface ISearchBarProps {
 export const SearchBar = ({ handleSearchBarSubmit }: ISearchBarProps) => {
     const [searchValue, setSearchValue] = useState('');
 
+    const handleClearButtonClick = () => {
+        setSearchValue('');
+    };
+
     return (
         <form className="search-bar__form" onSubmit={(event) => handleSearchBarSubmit(event, searchValue)}>
             {/* Input type="search" has clear input cross-button by default */}
             <input className="search-bar__input"
-                type="search"
+                type="text"
                 placeholder="Search..."
                 value={searchValue}
                 onChange={(event) => setSearchValue(event.target.value)} />
+            <button className="search-bar__clear-button"
+                type="reset"
+                title="Clear input"
+                onClick={handleClearButtonClick}>×</button>
         </form>
     );
 };
